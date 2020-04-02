66bf00c7b22a445089b2ebd7c8989833eeb5ee1e
kongkannika <kongkannika@gmail.com>
2020-04-02 22:45:11 +0700

    initialize project sample

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\Http\Resources\Post as PostResource;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate(20);
+        return PostResource::collection($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = $request->isMethod('put') ? Post::findOrFail($request->post_id) : new Post;
+
+        $post->id = $request->input('post_id');
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        if ($post->save()) {
+            return new PostResource($post);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id) ;
+
+        if ($post->delete()) {
+            return new PostResource($post);
+        }
+    }
+}
